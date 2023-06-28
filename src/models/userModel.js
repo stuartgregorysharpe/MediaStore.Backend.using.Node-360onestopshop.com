706351ffdf55eeb1cc86de15d0c5f86eff609549
CodeChief <codechief19900@gmail.com>
2023-06-28 04:54:57 +0100

    cloud

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -46,6 +46,9 @@ userSchema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema, 'users');
+
+// Uer: controller
+// users: database
 
 export default User;
